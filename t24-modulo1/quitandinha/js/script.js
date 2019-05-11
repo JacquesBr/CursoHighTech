7465fd57a32073b11df473c1bb105a29ef9a2363
Abulei <Abulei@localhost.localdomain>
2019-05-11 11:52:10 -0400

    aula 11/5 corigido

--- t24-modulo1/quitandinha/js/script.js
+++ t24-modulo1/quitandinha/js/script.js
@@ -23,28 +23,35 @@ function listarCesta() {
     carregarCesta()
     let lista = document.getElementById("listaCesta")
     for (let i = 0; i < cesta.length; i++) {
-        // alert(cesta[i])
+
+        alert(cesta[i])
+
         // Colocar os itens da cesta no HTML
-        let div = document.createElement("div")
-        div.classList.add("item-cesta")
-
-
+        // let div = document.createElement("div")div.classList.add("item-cesta")
         // criar span
-        let span = document.createElement("span")
-        span.innerHTML = cesta[i]
-        div.appendChild(span)
-
-
+        // // let span = document.createElement("span")span.innerHTML = cesta[i]div.appendChild(span)
         // criar botao
-        let button = document.createElement("button")
-        button.innerHTML = "Remover"
-        button.classList.add("btn")
-        button.addEventListener("click", remover(i))
-        button.classList.add("remover")
+        // let button = document.createElement("button")
+        // button.innerHTML = "Remover"
+        // button.classList.add("btn")
+        // button.addEventListener("click", remover(i))
+        // button.classList.add("remover")
+        // div.appendChild(button)
+        // lista.appendChild(div)
 
-        div.appendChild(button)
 
-        lista.appendChild(div)
-
+        let template = document.createElement("template")
+        template.innerHTML =
+            `<div class="item-cesta">
+            <span>${cesta[i]}</span>
+            <button onclick="remover(${i})">Remover</button>
+        </div>
+        `
+        lista.appendChild(template.content.childNodes[0])
     }
+}
+
+
+function remover(index) {
+    alert("Removendo o cara da posisao:" + index)
 }
